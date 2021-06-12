4b0868bbafc7e5ec42c94db5810e3c7007256740
hproctor021 <hproctor021@gmail.com>
2021-06-12 18:17:48 -0500

    started animation ques logic

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton){
+    constructor(durationInput, startButton, pauseButton, callbacks){
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if( callbacks ){
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         this.startButton.addEventListener('click', this.start);
         this.pauseButton.addEventListener('click', this.pause);
@@ -10,6 +15,9 @@ class Timer {
     }
 
     start = () => {
+        if( this.onStart ){
+            this.onStart();
+        }
         this.tick();
         this.interval = setInterval(this.tick, 1000);
         // by assigning the variable to this.timer, we can refer to this variable in other functions
@@ -25,8 +33,14 @@ class Timer {
         const timeRemaining = this.timeRemaining;
             if( this.timeRemaining <= 0 ){
             this.pause();
+            if( this.onComplete ){
+                this.onComplete();
+            }
          } else {
             this.timeRemaining = timeRemaining - 1;
+            if( this.onTick ){
+                this.onTick();
+            }
         }
     }
 
@@ -53,4 +67,14 @@ const startButton = document.querySelector('#start')
 const pauseButton = document.querySelector('#pause')
 const durationInput = document.querySelector('#duration')
 
-const timer = new Timer(durationInput, startButton, pauseButton)
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(){
+        console.log('onStart hit')
+    },
+    onTick(){
+        console.log('Timer ticked')
+    },
+    onComplete(){
+        console.log('Timer is completed')
+    }
+});
