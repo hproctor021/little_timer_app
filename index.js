1825ed422e264ce0a6f64e1b46bf826d17f6401d
hproctor021 <hproctor021@gmail.com>
2021-06-12 18:01:00 -0500

    added start button disablement & removed unused methods

--- index.js
+++ index.js
@@ -9,22 +9,43 @@ class Timer {
 
     }
 
-    durationInput = () => {
-
-    }
-
     start = () => {
         this.tick();
         this.interval = setInterval(this.tick, 1000);
         // by assigning the variable to this.timer, we can refer to this variable in other functions
-    }
-
-    tick = () => {
-        console.log('tick')
+        this.startDeactivate();
     }
 
     pause = () => {
         clearInterval(this.interval);
+        this.startActivate();
+    }
+
+    tick = () => {
+        const timeRemaining = this.timeRemaining;
+            if( this.timeRemaining <= 0 ){
+            this.pause();
+         } else {
+            this.timeRemaining = timeRemaining - 1;
+        }
+    }
+
+    startActivate = () => {
+        this.startButton.disabled = false;
+        console.log('start activated')
+    }
+
+    startDeactivate = () => {
+        this.startButton.disabled = true;
+        console.log('start disabled')
+    }
+
+    get timeRemaining(){
+        return parseFloat(this.durationInput.value)
+    }
+
+    set timeRemaining(time){
+       this.durationInput.value = time
     }
 }
 
